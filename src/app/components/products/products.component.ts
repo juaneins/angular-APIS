9ed99029ba19c3d9d20ad85b89037d2f9c3a8226
juaneins_uio <ballesterosjg@gmail.com>
2022-06-22 18:54:30 -0500

    add slides with swiper

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -15,6 +15,17 @@ export class ProductsComponent implements OnInit {
   total = 0;
   products: Product[] = [];
   showProductDetail: boolean = false;
+  productChosen: Product = {
+    id: '',
+    price: 0,
+    images: [],
+    title: '',
+    category: {
+      id: '',
+      name: '',
+    },
+    description: '',
+  };
 
   constructor(
     private storeService: StoreService,
@@ -41,6 +52,8 @@ export class ProductsComponent implements OnInit {
   onShowDetail(id: string) {
     this.productsService.getProduct(id).subscribe((data) => {
       console.log('product: ', data);
+      this.toggleProductDetail();
+      this.productChosen = data;
     });
   }
 }
