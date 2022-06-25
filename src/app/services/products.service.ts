57f579f5ebae24a3f9eadddf23b2fc62b5efd5da
juaneins_uio <ballesterosjg@gmail.com>
2022-06-25 14:01:53 -0500

    retry petitions

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
+import { retry } from 'rxjs/operators';
 
 import { Product } from './../models/product.model';
 import { CreateProductDTO, UpdateProductDTO } from '../models/productdto.model';
@@ -18,7 +19,7 @@ export class ProductsService {
       params = params.set('limit', limit);
       params = params.set('offset', offset);
     }
-    return this.http.get<Product[]>(this.apiUrl, { params });
+    return this.http.get<Product[]>(this.apiUrl, { params }).pipe(retry(3));
   }
 
   getProduct(id: string) {
