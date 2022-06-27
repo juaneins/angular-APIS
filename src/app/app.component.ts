428cbaed35614d81570fa47cbad82c0fc75e0a4b
juaneins_uio <ballesterosjg@gmail.com>
2022-06-26 20:04:54 -0500

    Login and auth

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,21 @@
 import { Component } from '@angular/core';
 
-import { Product } from './models/product.model';
+import { AuthService } from './services/auth.service';
+import { UsersService } from './services/users.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   imgParent = '';
   showImg = true;
 
+  constructor(
+    private authService: AuthService,
+    private usersService: UsersService
+  ) {}
 
   onLoaded(img: string) {
     console.log('log padre', img);
@@ -18,5 +23,23 @@ export class AppComponent {
 
   toggleImg() {
     this.showImg = !this.showImg;
+  }
+
+  createUser() {
+    this.usersService
+      .create({
+        name: 'Sebas',
+        email: 'sebas@mail.com',
+        password: '123456',
+      })
+      .subscribe((rta) => {
+        console.log(rta);
+      });
+  }
+
+  login() {
+    this.authService.login('sebas@mail.com', '123456').subscribe((rta) => {
+      console.log(rta.access_token);
+    });
   }
 }
